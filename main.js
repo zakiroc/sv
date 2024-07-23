5682a44bb7f8147a9e40ab6ee04266cfe239ceba
zakiroc <164227048+zakiroc@users.noreply.github.com>
2024-07-24 02:37:18 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
